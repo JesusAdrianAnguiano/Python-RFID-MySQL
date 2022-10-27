dc720361c85a483118cdf056a419526eef409f80
Jesús Adrián Anguiano Velázquez <jesusadriananguiano2001@hotmail.com>
2022-10-27 13:05:41 -0500

    Agregué comillas a las strings del codigo mysql-python

--- Test-mysql-python.py
+++ Test-mysql-python.py
@@ -1,13 +1,13 @@
 
 from mysql.connector import (connection)
-print(conectandose a la base de datos)
+print("conectandose a la base de datos")
 cnx = connection.MySQLConnection(user='adrian', password='1234',
                                  host='127.0.0.1',
                                  database='coviddetector')
-print(conexión realizada)
+print("conexión realizada")
 print(cnx)
 
-print(cerrar conexión)
+print("cerrar conexión")
 cnx.close()
-print(conexión cerrada)
+print("conexión cerrada")
 
