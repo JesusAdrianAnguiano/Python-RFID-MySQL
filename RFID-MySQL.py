d8e50c809687d6fe8b41b034bd3bfb1bee4f0324
Jesús Adrián Anguiano Velázquez <jesusadriananguiano2001@hotmail.com>
2022-10-28 17:19:08 -0500

    Modificación al codifo rfid

--- RFID-MySQL.py
+++ RFID-MySQL.py
@@ -1,3 +1,4 @@
+
 # Bibliotecas
 from time import sleep
 import sys
@@ -5,12 +6,10 @@ from mfrc522 import SimpleMFRC522
 import RPi.GPIO as GPIO
 import mysql.connector
 
-print("Conectando")
-cnx = mysql.connector.connect(user='adrianlan', password='1234',
+cnx = mysql.connector.connect(user='adrianln', password='1234',
                                  host='192.168.100.157',
                                  database='RFID')
-cursor = cnx.cursor()
-print("Conectado a la base de datos")
+
 
 # Inicar el sensor
 reader = SimpleMFRC522()
@@ -22,19 +21,14 @@ try:
         print("Acercar el tag al lector")
         id, text = reader.read()
         print("ID: %s\nText: %s" % (id,text))
+        cursor = cnx.cursor()
+        query = ("INSERT INTO rfid (texto,rfid) VALUES ('"+ text +"','"+ id +"');")
+        cursor.execute(query)
+        cnx.commit()
         sleep(5)
 except KeyboardInterrupt:
     GPIO.cleanup()
     raise
-
-query = ("INSERT INTO rfid (texto,rfid) VALUES ("%s %s % (id,text)";")
-
-
-cursor.execute(query)
-cnx.commit()
-
-cursor.close()
-cnx.close()
-
-
+    cursor.close()
+    cnx.close()
 
