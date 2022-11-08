90c0f1bd7e4e1fddd3c23f79306180180b4c9e9f
Jesús Adrián Anguiano Velázquez <jesusadriananguiano2001@hotmail.com>
2022-11-08 11:41:09 -0600

    Modifiqué el codigo para representar su ejecución en host y en lan

--- Query-mysql.py
+++ Query-mysql.py
@@ -1,8 +1,10 @@
 
 import mysql.connector
 
-cnx = mysql.connector.connect(user='adrian', password='1234',
-                                 host='127.0.0.1',
+cnx = mysql.connector.connect(user='adrianlan', password='1234',
+                                 #user='adrian', password='1234', correr en máquina virtual
+                                 #host='127.0.0.1', correr en máquina virtual
+                                 host='192.168.100.157',
                                  database='coviddetector')
 cursor = cnx.cursor()
 query = ("SELECT id, nombre, temp FROM registro WHERE temp>40")
